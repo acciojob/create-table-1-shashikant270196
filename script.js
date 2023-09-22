a6a52d0e325295c57a36443becb89ac72738a982
Shashikant yadav <127616023+shashikant270196@users.noreply.github.com>
2023-09-22 20:36:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
-function insert_Row() {
+document.getElementById("button").onclick=function insert_Row() {
 	const tr = document.CreateElement("tr");
 	tr.innerHTML=`<td>${new cell}</td>`;
 	sampleTable.appendchild(tr); 
 }
+inert_Row();
